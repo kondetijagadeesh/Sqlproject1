aad28073711de8fd4c7d2b8f4e68ea072c00d048
unknown <kondetijagadeesh38@gmail.com>
2022-10-12 23:17:49 +0530

    some thinglmkdlckclnd

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+USE SQL_TRAINING
+GO
+
+CREATE TABLE TABLE_DATATYPES (ID INT)
+GO
+
+What IS datatype..?
+
+Numbers
+Charecter string OR Alpha numeric values--> Alphabets and numeric -- 123456789, abcd...Z
+
+B.tech NO. 0af1234nz
+
+Datatime 
+
+Types OF datatypes
+1. Numeric datatype --Only Number store
+
+Bit --> it stores only 0 or 1 numbers -- 1 Byte --64 KB SPace
+Tinyint --> It stores range of 0 to 255 number -- -- 1 Byte --64 KB Space
+Smallint --> It stores range of 0 to 32767 number -- 2 Byte --128 KB Space
+Int --> --> It stores range of 0 to 2147483647 number -- 4 Byte --256 KB Space
+Bigint --> 
+Decimal(Size, Decimal) --> Money --> 20.60, 50.20 --> 
+
+Real Number OR Number OR interger 20.50
+.50 --> decimal value
+
